a493d532f5f6ddcc908c685c4904c8af8355b5bf
joxoleon <jovan.radivojsa@gmail.com>
2019-04-13 14:24:56 +0200

    Create test project and add initial version of templates.

---
+++ Example/CleanSwiftTestProject/CleanSwiftTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+////  ViewController.swift
+//  CleanSwiftTestProject
+//
+//  Created by Dejan on 4/13/19.
+//  Copyright © 2019 Jovan. All rights reserved. Dejan Borojevic
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
