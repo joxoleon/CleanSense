02b8c743848db00723161bd0e318cccc0acd40e4
joxoleon <jovan.radivojsa@gmail.com>
2019-04-13 20:32:50 +0200

    Updated .gitignore not to push Pods.

--- Example/Pods/Target Support Files/Pods-CleanSwiftTestProject/Pods-CleanSwiftTestProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CleanSwiftTestProject : NSObject
-@end
-@implementation PodsDummy_Pods_CleanSwiftTestProject
-@end
--- Example/Pods/Target Support Files/Pods-CleanSwiftTestProjectTests/Pods-CleanSwiftTestProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CleanSwiftTestProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_CleanSwiftTestProjectTests
-@end
